0fd0029162027fe5074e5c291b8ade172ad3b78a
AhmedAbdelaziz0 <ahmed2588547@gmail.com>
2023-12-24 17:35:15 +0200

    add: add a file to generate prefix tree with Huffman

---
+++ include/PrefixCode.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace PrefixCode {
+
+std::vector<std::string> getCodePrefix(const std::string &text);
+} // namespace PrefixCode
---
+++ src/PrefixCode.cpp
@@ -0,0 +1,31 @@
+#include "PrefixCode.hpp"
+#include "HuffmanTree.hpp"
+
+static std::vector<int> countCharacters(const std::string &str) {
+  std::vector<int> counts(256, 0);
+  for (const auto &ch : str) {
+    counts[ch]++;
+  }
+  return counts;
+}
+
+static HuffmanTree::PriorityQueue makeQueue(const std::vector<int> &counts) {
+  HuffmanTree::PriorityQueue queue;
+  for (int i = 0; i < counts.size(); i++) {
+    if (counts[i] > 0) {
+      queue.push(std::shared_ptr<HuffmanTree::HuffNode>(
+          new HuffmanTree::HuffLeafNode(counts[i], i)));
+    }
+  }
+  return queue;
+}
+
+std::vector<std::string> PrefixCode::getCodePrefix(const std::string &text) {
+  std::vector<int> occurrence = countCharacters(text);
+  auto queue = makeQueue(occurrence);
+  auto huff_tree = HuffmanTree::buildHuffTree(queue);
+  std::string holder;
+  std::vector<std::string> codes(256);
+  HuffmanTree::GetPrefixTable(huff_tree, holder, codes);
+  return codes;
+}
