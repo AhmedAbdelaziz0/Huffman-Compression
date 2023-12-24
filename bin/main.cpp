3338e34a5b5e0967f1dcb49f334c1d34d8b4c01f
AhmedAbdelaziz0 <ahmed2588547@gmail.com>
2023-12-24 14:40:26 +0200

    move code to headers and cpp

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,53 +1,28 @@
-#include "HuffmanTree.hpp"
-#include "UserInputHandler.hpp"
-#include <fstream>
-#include <iostream>
-#include <memory>
-#include <vector>
+#include "Compression.hpp"
+#include "ParseArgs.hpp"
+#include "PrefixCode.hpp"
+#include "FileHandler.hpp"
 
-std::vector<int> countCharacters(const std::string &str) {
-  std::vector<int> counts(256, 0);
-  for (const auto &ch : str) {
-    counts[ch]++;
-  }
-  return counts;
-}
+int main(int argc, char **argv) {
+  Parser::Arguments arguments = Parser::parseArguments(argc, argv);
 
-HuffmanTree::PriorityQueue makeQueue(const std::vector<int> &counts) {
-  HuffmanTree::PriorityQueue queue;
-  for (int i = 0; i < counts.size(); i++) {
-    if (counts[i] > 0) {
-      queue.push(std::shared_ptr<HuffmanTree::HuffNode>(
-          new HuffmanTree::HuffLeafNode(counts[i], i)));
-    }
-  }
-  return queue;
-}
+  if (arguments.isCompress) {
+    std::string original_text =
+        FileHandler::loadFileAsString(arguments.input_file);
+    std::vector<std::string> code_map =
+        PrefixCode::getCodePrefix(original_text);
+    std::string compressed = Compression::compress(original_text, code_map);
+    std::string compined = Compression::compineCodeMap(code_map);
+    FileHandler::saveFile(compined + compressed, arguments.output_file);
+  } else {
+    std::string compressed_text =
+        FileHandler::loadFileAsString(arguments.input_file);
+    std::vector<std::string> code_map = Compression::splitCompressedText(compressed_text);
 
-template <typename T> void Log(const std::vector<T> &vec) {
-  for (const auto &v : vec) {
-    std::cout << v << ", ";
-  }
-  std::cout << '\n';
-}
-
-void Log(const std::string &str) { std::cout << str << std::endl; }
-
-// TODO get from command line
-// TODO write function or class for the input from command line ( getFileFromUser )
-int main() {
-  std::ifstream text_file = UserInputHandler::getFileFromUser();
-  std::string text = UserInputHandler::toString(text_file);
-  text.pop_back();
-
-  std::vector<int> occurrence = countCharacters(text);
-  auto queue = makeQueue(occurrence);
-  auto huff_tree = HuffmanTree::buildHuffTree(queue);
-  std::string temp;
-  std::vector<std::pair<char, std::string>> codes;
-  HuffmanTree::GetPrefixTable(huff_tree, temp, codes);
-  for(auto c: codes) {
-    std::cout << c.first << " " << c.second << std::endl;
+    compressed_text = code_map.back();
+    code_map.pop_back();
+    std::string original_text = Compression::decompress(compressed_text, code_map);
+    FileHandler::saveFile(original_text, arguments.output_file);
   }
 
   return 0;
